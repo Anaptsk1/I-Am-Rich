c3e0caa00a8afca2f5931746ff6baac3a8fc1b5e
Ana Ptskialadze <ani@Anis-MBP.fritz.box>
2023-10-14 21:07:54 +0200

    first commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ana Ptskialadze on 17.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
